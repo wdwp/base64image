129ecca3df0c0e45acf9223f243ce8a74d2cef15
Yuri <wdwp@bk.ru>
2021-11-29 19:18:47 +0300

    Add files via upload

--- function.base64image.php
+++ function.base64image.php
@@ -24,68 +24,64 @@
 
 function smarty_function_base64image($params, &$smarty)
 {
+	$gCms = CmsApp::get_instance();
+	$root_path = $gCms->config['root_path'];
+	$root_url =	$gCms->config['root_url'];
+
 	#Get parameters from the function call
-    $src = isset($params['src']) ? $params['src'] : '';
+	$src = isset($params['src']) ? str_replace(array($root_url . '/', '//'), array('', '/'), $params['src']) : '';
 
-    if (isset($src) && !empty($src)) {
+	if (isset($src) && !empty($src)) {
 
-    	$config = CmsApp::get_instance()->GetConfig();
-    	$root = $config->smart_root_url();
+		$path = $root_path . '/' . urldecode($src);
 
-    	#Ignoring SSL errors
-    	$arrContextOptions=array(
-    		"ssl"=>array(
-    			"verify_peer"=>false,
-    			"verify_peer_name"=>false,
-    		),
-    	);
+		#Get source data
+		$data = file_get_contents($path);
+	} //end if
 
-    	#Get source data
-    	$data = file_get_contents($root.'/'.$src, false, stream_context_create($arrContextOptions));
+	if (isset($data) && !empty($data)) {
 
-    } //end if
+		#Get source path
+		$pathinfo = pathinfo($path);
 
-   	if(isset($data) && !empty($data)) {
+		#Get source type
+		$type = strtolower($pathinfo['extension']);
 
-        #Get source path
-  	    $path = parse_url($src, PHP_URL_PATH);
+		#Return encoded image
+		return 'data:image/' . $type . ';base64,' . base64_encode($data);
+	} else {
 
-   		#Get source type
-   		$type = pathinfo($path, PATHINFO_EXTENSION);
+		return 'Error: image path is not valid';
+	} //end if
 
-   		#Return encoded image
-   		return 'data:image/' . $type . ';base64,' . base64_encode($data);
-
-   	} else {
-
-   		return 'Error: image path is not valid';
-
-   	} //end if
-
-}// End Function
+} // End Function
 /**
  * Help text
  */
-function smarty_cms_help_function_base64image() {
-         ?>
-                  <h3>What does this do?</h3>
-                  <p>This plugin encodes an image to base64 format.</p>
+function smarty_cms_help_function_base64image()
+{
+?>
+	<h3>What does this do?</h3>
+	<p>This plugin encodes an image to base64 format.</p>
 
-                  <h3>How do I use it?</h3>
-                  <p>HTML: &lt;img src="{base64image src="uploads/simplex/images/cmsmadesimple-logo.png"}" width="227" height="59" alt="{sitename}"/&gt;</p>
-                  <p>CSS: background: url([[base64image src="uploads/simplex/images/palm-circle.png"]]) no-repeat;</p>
-         <?php
+	<h3>How do I use it?</h3>
+	<p>HTML: &lt;img src="{base64image src="uploads/simplex/images/cmsmadesimple-logo.png"}" width="227" height="59" alt="{sitename}"/&gt;</p>
+	<p>CSS: background: url([[base64image src="uploads/simplex/images/palm-circle.png"]]) no-repeat;</p>
+	<p>You can use an absolute and relative url for the image src.</p>
+<?php
 } // End Function
 /**
  * About text
  */
-function smarty_cms_about_function_base64image() {
-        ?>
-                  <p><b>Plugin author: Yuri Haperski (wdwp@yandex.ru)</b></p>
+function smarty_cms_about_function_base64image()
+{
+?>
+	<p><b>Plugin author: Yuri Haperski (wdwp@yandex.ru)</b></p>
 
-                  <p><b>Version:</b> 1.0</p>
-                  <p><b>Change History:</b></p>
-                  <p><b>18-03-2021 - Initial release (v1.0)</b></p>
-        <?php
+	<p><b>Version:</b> 1.1</p>
+	<p><b>Change History:</b></p>
+	<p><b>18-03-2021 - Initial release (v1.0)</b></p>
+	<p><b>29-11-2021 - Some improvements</b></p>
+<?php
 } // End Function
 ?>
